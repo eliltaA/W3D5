d405007ae9ef166c382b3d28a3c2d31eb7a666ee
ncar285 <carlier.nicholas@gmail.com>
2023-06-09 15:23:30 -0700

    add dfs

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -15,6 +15,35 @@ class PolyTreeNode
    
   end
 
-end
+  def add_child(child) # child = node3 ,,, self = node2
 
-#node2.parent = node1
+    child.parent = self   #CHNAGE node3's parent to node2
+
+    @children << child  if !self.children.include?(child) # add node3 into node2's children list
+    
+  end
+
+  def remove_child(child)
+    parent = self
+    if !parent.children.include?(child)
+      raise
+    end
+      if parent 
+      parent.children.delete(child)  #
+      child.parent = nil 
+    end
+  end
+
+
+  def dfs(target_value)
+    return self if self.value == target_value   # if node 1 happend to be the target
+      self.children.each do |child|
+        found = child.dfs(target_value)
+        return found if found
+      end
+      return nil
+  end
+
+  
+
+end
