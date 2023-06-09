f6cbd81c02dd952be03e5bc5cb403765bbf61daf
“eliltaA” <eliltatabrham@gmail.com>
2023-06-09 13:54:32 -0700

    Add Parent

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,3 +1,24 @@
 class PolyTreeNode
-  
+    attr_reader :parent, :children, :value
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(new_parent)
+    p self
+    p self.parent.children
+    if self.parent != nil
+        p self
+        p self.parent.children
+        self.parent.children.delete(self)
+    end
+    #self.parent = new_parent
+    new_parent.children << self if new_parent && !new_parent.children.include?(self)
+   
+  end
+
 end
+
+#node2.parent = node1
