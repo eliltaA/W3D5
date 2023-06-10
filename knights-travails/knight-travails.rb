58f8249ca043584109b5d093edd641e2b4ba81e9
“eliltaA” <eliltatabrham@gmail.com>
2023-06-09 18:30:57 -0700

    add build_move_tree

--- knights-travails/knight-travails.rb
+++ knights-travails/knight-travails.rb
@@ -27,29 +27,35 @@ class KnightPathFinder
 
     end
 
-
-
-
-
-
-
-
-
-
-
-
-
     def initialize(pos)
-        # @current_position = pos
+        @start_position = pos
         @board = Array.new(8) {Array.new(8, [])}
         @root_node = PolyTreeNode([pos])
+        @considered_positions = [pos]
         # @move_tree = 
     end
 
-    # def build_move_tree
-    #     self.root_node
-    #     ()
+    def new_move_positions(pos)
+    options = KnightPathFinder.valid_moves(pos).select {|pos| !@considered_positions.include?(pos)}
+    @considered_positions += options
+    return options
+    end
+    
+    def build_move_tree
+        q = Queue.new
+        q.enq(@root_node)
+        until q.empty?
+            current_node = q.deq
+            current_pose = current_node.value
+            return current_node if current_node.value == target_value
+            current_node.chidren = new_move_positions(current_pose)
+            current_node.children.each do |child|
+            q.enq(child)
+            end
+        end
+        nil
+      end
+    end
 
-    # end
-
+    
 end
